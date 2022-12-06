630055f4a17ded78861a8aa0657451a87052c640
Gaël Bouquain <gael.bouquain@gmail.com>
2022-12-06 15:02:09 +0100

    2022 / Upgrade 6th day

--- 2022/Days/Day6.fs
+++ 2022/Days/Day6.fs
@@ -1,34 +1,18 @@
 module Day6
 
-open System.Collections.Generic
-
-let input = System.IO.File.ReadAllText "./Inputs/Day6.txt"
-
-let readMarker = Stack<char>()
-
-let getLastStackItem quantity stack =
-    stack
-    |> List.ofSeq
-    |> List.chunkBySize quantity
-    |> List.tryHead
-    |> Option.defaultValue []
-
-let findMarkerFromLength markerLength input =
+let findMarkerFromLength markerLength (input: string) =
     input
     |> Seq.indexed
-    |> Seq.choose (fun (index, char) ->
-        readMarker
-        |> getLastStackItem markerLength
-        |> List.distinct
-        |> List.length
-        <> markerLength
-        |> function
-            | true ->
-                readMarker.Push char
-                None
-            | false -> Some index
+    |> Seq.windowed markerLength
+    |> Seq.tryFind (fun window ->
+        window
+        |> Seq.distinctBy snd
+        |> Seq.length = markerLength
     )
-    |> Seq.head
+    |> Option.map (Seq.last >> fst >> (+) 1)
 
-printfn "Part 1 -> %A" (input |> findMarkerFromLength 4)
-printfn "Part 2 -> %A" (input |> findMarkerFromLength 14)
+"./Inputs/Day6.txt"
+|> System.IO.File.ReadAllText
+|> fun input ->
+    printfn "Part 1 -> %A" (input |> findMarkerFromLength 4)
+    printfn "Part 2 -> %A" (input |> findMarkerFromLength 14)
