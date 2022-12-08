2eada9bbd6e17e2c62949db5efcc6b92ab6f6f31
Gaël Bouquain <gael.bouquain@gmail.com>
2022-12-08 21:53:11 +0100

    2022 / Upgrade 8th day

--- 2022/Days/Day8.fs
+++ 2022/Days/Day8.fs
@@ -1,20 +1,29 @@
 module Day8
 
 type Matrix<'a> = 'a list list
+module Matrix =
+    let getColumn index (matrix: _ Matrix) =
+        matrix |> List.map (fun line -> line.[index])
+
+    let map f (matrix: _ Matrix) : _ Matrix = matrix |> List.map (List.map f)
+
+    let locateAllElements (matrix: _ Matrix) : _ Matrix =
+        matrix
+        |> List.mapi (fun lineIndex line ->
+            line
+            |> List.mapi (fun columnIndex element -> element, (lineIndex, columnIndex)))
 
 let inline charToInt c = int c - int '0'
 
-let treeMatrix: int Matrix =
+let treeMatrix =
     System.IO.File.ReadAllLines "./Inputs/Day8.txt"
     |> List.ofArray
     |> List.map Seq.toList
     |> List.map (List.map charToInt)
 
-let getMatrixColumn index (matrix: _ Matrix) =
-    matrix |> List.map (fun line -> line.[index])
 
-let isTreeVisible (lineIndex: int) (columnIndex: int) treeMatrix =
-    let column = treeMatrix |> getMatrixColumn columnIndex
+let getTreeCrossingLines (lineIndex: int, columnIndex: int) treeMatrix =
+    let column = treeMatrix |> Matrix.getColumn columnIndex
     let line = treeMatrix |> List.item lineIndex
     let tree = line |> List.item columnIndex
 
@@ -26,23 +35,24 @@ let isTreeVisible (lineIndex: int) (columnIndex: int) treeMatrix =
         column |> List.splitAt lineIndex |> fst,
         column |> List.splitAt (lineIndex + 1) |> snd
 
-    lineLeft |> List.filter (fun x -> x >= tree) |> List.isEmpty
-    || lineRight |> List.filter (fun x -> x >= tree) |> List.isEmpty
-    || columnTop |> List.filter (fun x -> x >= tree) |> List.isEmpty
-    || columnBottom |> List.filter (fun x -> x >= tree) |> List.isEmpty
+    tree, lineLeft, lineRight, columnTop, columnBottom
 
-let getTreeScenicScore (lineIndex: int) (columnIndex: int) treeMatrix =
-    let column = treeMatrix |> getMatrixColumn columnIndex
-    let line = treeMatrix |> List.item lineIndex
-    let tree = line |> List.item columnIndex
 
-    let lineLeft, lineRight =
-        line |> List.splitAt columnIndex |> fst,
-        line |> List.splitAt (columnIndex + 1) |> snd
+let isTreeVisible (lineIndex: int, columnIndex: int) treeMatrix =
+    let tree, lineLeft, lineRight, columnTop, columnBottom =
+        getTreeCrossingLines (lineIndex, columnIndex) treeMatrix
 
-    let columnTop, columnBottom =
-        column |> List.splitAt lineIndex |> fst,
-        column |> List.splitAt (lineIndex + 1) |> snd
+    [ lineLeft
+      lineRight
+      columnTop
+      columnBottom ]
+    |> List.map (List.filter (fun x -> x >= tree)) // Check if all parts of line contain tallest trees than the current.
+    |> List.map List.isEmpty // Check if there are parts of line that contain only trees smallest that the current.
+    |> List.contains true
+
+let getTreeScenicScore (lineIndex: int, columnIndex: int) treeMatrix =
+    let tree, lineLeft, lineRight, columnTop, columnBottom =
+        getTreeCrossingLines (lineIndex, columnIndex) treeMatrix
 
     [ lineLeft |> List.rev
       lineRight
@@ -55,22 +65,18 @@ let getTreeScenicScore (lineIndex: int) (columnIndex: int) treeMatrix =
 
 let part1 =
     treeMatrix
-    |> List.mapi (fun lineIndex treeLine ->
-        treeLine |> List.mapi (fun columnIndex _ ->
-            isTreeVisible lineIndex columnIndex treeMatrix
-        )
-    )
+    |> Matrix.locateAllElements
+    |> Matrix.map snd
+    |> Matrix.map (fun pos -> isTreeVisible pos treeMatrix)
     |> List.concat
     |> List.filter id
     |> List.length
 
 let part2 =
     treeMatrix
-    |> List.mapi (fun lineIndex treeLine ->
-        treeLine |> List.mapi (fun columnIndex _ ->
-            getTreeScenicScore lineIndex columnIndex treeMatrix
-        )
-    )
+    |> Matrix.locateAllElements
+    |> Matrix.map snd
+    |> Matrix.map (fun pos -> getTreeScenicScore pos treeMatrix)
     |> List.concat
     |> List.max
 
