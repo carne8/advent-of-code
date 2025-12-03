b9512e63cdca3a2e1028d01716b8c3c501293d20
Gaël Bouquain <gael.bouquain@gmail.com>
2025-12-03 22:18:26 +0100

    2025 - Day 3

---
+++ 2025/Day03.fs
@@ -0,0 +1,54 @@
+﻿module AOC._2025.Day03
+
+open System
+open System.IO
+
+let input =
+    "input.txt"
+    |> File.ReadAllLines
+    |> Array.map (fun bank -> bank :> char seq)
+
+let findPairOfBank bank =
+    let b =
+        bank
+        |> Seq.indexed
+        |> Seq.toArray
+
+    let n = b |> Array.length
+
+    let firstDigitIdx, firstDigit =
+        b
+        |> Array.take (n-1)
+        |> Array.maxBy (snd >> Char.GetNumericValue >> int)
+    let _, secondDigit =
+        b
+        |> Array.skip (firstDigitIdx + 1)
+        |> Array.maxBy (snd >> Char.GetNumericValue >> int)
+
+    sprintf "%c%c" firstDigit secondDigit |> int
+
+let partOne () = input |> Array.sumBy findPairOfBank
+
+let rec findBatteries n (bank: int64 list) =
+    match n with
+    | 0 -> []
+    | _ ->
+        let length = bank |> List.length
+        let maxIdx, max =
+            bank
+            |> List.indexed
+            |> List.take (length-n+1)
+            |> List.maxBy snd
+
+        printfn "%A" (bank, max)
+
+        max :: findBatteries (n-1) bank[maxIdx+1..]
+
+let partTwo () =
+    input |> Array.sumBy (
+        Seq.map (Char.GetNumericValue >> int64)
+        >> Seq.toList
+        >> findBatteries 12
+        >> List.fold (fun acc e -> acc + string e) ""
+        >> int64
+    )
